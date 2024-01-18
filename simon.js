668e52f3fc627c21988b31294c9a42b20685cdfd
wickhill <wickhill@gmail.com>
2024-01-18 15:29:57 -0500

    Had some spare time and added a few more features

--- simon.js
+++ simon.js
@@ -1,5 +1,19 @@
-document.querySelectorAll(".gameBtn").forEach(function (button) {
-  button.addEventListener("click", function () {
-    //Placeholder for Game / Button Logic...
+// CoolDogGumBone aka 'Simon' bulk framework:
+
+function initiateGame() {
+
+  document.querySelectorAll(".letsBegin").forEach(function (button) {
+    button.addEventListener("click", function () {
+      //Placeholder for Game / Button Logic...
+    });
   });
-});
+
+  const buttonChoices = ["cool", "dog", "gum", "bone"];
+  const compButtonArray = [];
+  const playerButtonArray = [];
+
+  for (let i = 0; i < buttonChoices.length; i++) {
+    const randomNum = Math.floor(Math.random) * buttonChoices.length + 1;
+    compButtonArray.push(randomNum);
+  }
+}
