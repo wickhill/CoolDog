4939392c9346719fb087ec85b99a9c4545826946
wickhill <wickhill@gmail.com>
2024-01-24 13:42:38 -0500

    Got the hint button working now to make it green yellow red depending on how many hints left

--- simon.js
+++ simon.js
@@ -60,6 +60,7 @@ let sfx = {
 let compButtonArray = [];
 let currentScore = 0;
 let highScore = 0;
+let hintCounter = 0;
 // I initialized / declared 'let isPlayerTurn = true;' because - for some reason - starting it off as 'false' was making it perform in unexpected ways. But this method, starting with it declared as 'true' works just fine! More testing is required, but so far, so good!
 let isPlayerTurn = true;
 
@@ -69,10 +70,12 @@ document.getElementById("highScoreNums").innerHTML = highScore.toString();
 function startGame() {
   compButtonArray = [];
   let currentScore = 0;
+  hintCounter = 0;
   document.getElementById("startGameBtn").innerHTML = "Start New Game";
   document.getElementById("currentScoreNums").innerHTML =
     currentScore.toString();
   btnContainer.classList.remove("allButtonsIncorrect");
+  document.getElementById("hintBtn").innerHTML = "Hints Remaining: Three";
   document.getElementById("coolBtn").innerHTML = buttonEmojis[0].emoji;
   document.getElementById("dogBtn").innerHTML = buttonEmojis[1].emoji;
   document.getElementById("gumBtn").innerHTML = buttonEmojis[2].emoji;
@@ -207,7 +210,9 @@ function resetGame() {
 document.getElementById("resetBtn").addEventListener("click", resetGame);
 
 document.getElementById("hintBtn").addEventListener("click", function () {
+  hintCounter++;
   console.log(compButtonArray);
+  numberOfHints();
 });
 
 document.getElementById("startGameBtn").addEventListener("click", startGame);
@@ -283,6 +288,32 @@ document.querySelector("#boneBtn").addEventListener("click", function (e) {
     console.log(sfx.bone.play("segment"));
   }
 });
+
+//
+
+// Here's the 'hint button' function and limitation feature:
+
+function numberOfHints() {
+  if (hintCounter === 0) {
+    document.getElementById("hintBtn").innerHTML = "Hints Remaining: Three";
+  } else if (hintCounter === 1) {
+    document.getElementById("hintBtn").innerHTML = "Hints Remaining: Two";
+  } else if (hintCounter === 2) {
+    document.getElementById("hintBtn").innerHTML = "Hints Remaining: One";
+  } else if (hintCounter === 3) {
+    document.getElementById("hintBtn").innerHTML = "Don't Do It!";
+  } else if (hintCounter === 4) {
+    document.getElementById("hintBtn").innerHTML = "Don't push your luck!";
+  } else if (hintCounter === 5) {
+    document.getElementById("hintBtn").innerHTML = "We really mean it!";
+  } else {
+    document.getElementById("hintBtn").innerHTML =
+      "Don't say we didn't warn ya!";
+    correctOrIncorrect(false);
+    resetGame();
+    return;
+  }
+}
 
 // let gameStartInterval = setInterval(cpuSelects, 2000);
 
--- wireframe-simon.js
+++ wireframe-simon.js
@@ -1,13 +1,37 @@
 // Time to set a limit on hint button input! Uh, I mean, 'clicks'
 // <button id="hintBtn">Hints Remaining: Three</button>
 
-var counter = 0;
+hintCounter++;
 
-function a() {
-  if (counter < 5) {
-    document.getElementById("a").innerHTML += "<input type='radio'>";
-    counter++;
-  } else {
-    document.getElementById("id").disabled = true;
+let hintCounter = 0;
+
+function numberOfHints() {
+  if (hintCounter <= 0) {
+    document.getElementById("hintBtn").innerHTML = "Hints Remaining: Three";
   }
+  if (hintCounter === 1) {
+    document.getElementById("hintBtn").innerHTML = "Hints Remaining: Two";
+  }
+  if (hintCounter === 2) {
+    document.getElementById("hintBtn").innerHTML = "Hints Remaining: One";
+  }
+  if (hintCounter === 3) {
+    document.getElementById("hintBtn").innerHTML = "Don't Do It!";
+  }
+  if (hintCounter === 4) {
+    document.getElementById("hintBtn").innerHTML = "Don't push your luck!";
+  }
+  if (hintCounter === 5) {
+    document.getElementById("hintBtn").innerHTML = "We really mean it!";
+  } else {
+    document.getElementById("hintBtn").innerHTML =
+      "Don't say we didn't warn ya!";
+    correctOrIncorrect(false);
+    resetGame();
+    return;
+  }
+}
+
+function checkhintCounter() {
+  return hintCounter;
 }
